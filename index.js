1554fe035d307e3e36cbabb11052101346be9a3c
victorjunior95 <victorjr.muchacho@gmail.com>
2021-07-07 15:57:55 -0300

    iniciando o projeto Store Manager. VAMOS COM TUDO! :rocket:

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, console.log(`Server running in port ${PORT}`));
