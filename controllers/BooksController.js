322caecbce0fc51b0d69c17660a28a3931bf16f6
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-07 14:31:04 -0300

    Começando o projeto - trazendo padronizaçao de arquivos que serao modificados

---
+++ controllers/BooksController.js
@@ -0,0 +1,68 @@
+
+const Joi = require('joi');
+const rescue = require('express-rescue');
+const Book = require('../services/Book');
+
+const getAll = rescue(async (req, res) => {
+  const books = await Book.getAll();
+  res.status(200).json(books);
+});
+
+const findById = rescue(async (req, res, next) => {
+  const { id } = req.params;
+
+  const book = await Book.findById(id);
+
+  if (book.error) return next(book.error);
+
+  res.status(200).json(book);
+});
+
+const findByAuthorId = rescue(async (req, res, next) => {
+    const { id } = req.params;
+  
+    const book = await Book.findByAuthorId(id);
+  
+    if (book.error) return next(book.error);
+  
+    res.status(200).json(book);
+  });
+
+ 
+const findByTitle = rescue(async (req, res, next) => {
+    const { title } = req.params;
+  console.log(title)
+    const book = await Book.findByTitle(title);
+    console.log(title)
+    if (book.error) return next(book.error);
+  
+    res.status(200).json(book);
+  });
+
+  
+
+const create = rescue(async (req, res, next) => {
+  const { error } = Joi.object({
+    title: Joi.string().not().empty().required(),
+    authorId: Joi.string().not().empty().required(),
+  })
+    .validate(req.body);
+
+  if (error) return next(error);
+
+  const { title, authorId } = req.body;
+
+  const newBook = await Book.create(title, authorId);
+
+  if (newBook.error) return next(newBook.error);
+
+  res.status(201).json(newBook);
+});
+
+module.exports = {
+  getAll,
+  findById,
+  findByAuthorId,
+  findByTitle,
+  create,
+};
