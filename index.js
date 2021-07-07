60771a40a27f64de875cf3d6b699b60610299926
brenno-calado <brenno.calado@gmail.com>
2021-07-07 16:24:49 -0300

    product initial route

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', require('./router/product'));
+
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
