50211b10510f69e5aba62694e3e5da472b55ba25
Saulo Kirchmaier <saulokirchmaier@gmail.com>
2021-07-07 17:14:40 -0300

    Initial configs, start server and mongodb connection

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`O PAI TA ON PORT ${PORT}!`));
