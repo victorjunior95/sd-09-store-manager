d6410b1d48737c7314824e66aa8dae42c78928b0
BernardoFontes <bfontes.1997@gmail.com>
2021-07-07 16:05:35 -0300

    a

--- models/connection.js
+++ models/connection.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
-//a
+//ab
 const DB_NAME = 'StoreManager';
 
 const connection = () => {
