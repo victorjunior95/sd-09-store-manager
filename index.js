a74e89701cafd06495f215a323c62006e8607077
Lucas Silva <martssilvapro@gmail.com>
2021-07-07 15:56:41 -0300

    adiciona estrutura da api e conexão com o servidor

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+const HTTP_OK_STATUS = 200;
+const port = 3000;
+
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
