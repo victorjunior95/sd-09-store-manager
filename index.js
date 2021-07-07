c05f58924f34834bae3e16f7a28b5f063e82bde9
tiago-yoneda <xyonedax@gmail.com>
2021-07-07 13:58:00 -0300

    Tiago Yoneda - Project Store Manager - Initial Commit

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+require('dotenv').config();
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
