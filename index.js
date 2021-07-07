4d0e0d8654dc6ecb72f71c04369c0a42bc0151b4
Raquel Picanço <picanco.raquel@gmail.com>
2021-07-07 14:04:02 -0400

    First commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
