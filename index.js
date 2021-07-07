8bb84175a9b395f71f65d2735a2956ebc22e945d
Felipe <felipesantiagolds@gmail.com>
2021-07-07 16:18:58 -0300

    Create server

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`> Server is up and running on PORT : ${PORT}`));
