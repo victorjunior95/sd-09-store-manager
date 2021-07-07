f4d7db1d0685c09de67304b0e4cbafacdaee6e3d
Rafael Pufal <rafael.pufal23@gmail.com>
2021-07-07 19:12:17 -0300

    req 1 ok vai vai

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,6 @@
 const { MongoClient } = require('mongodb');
-
-// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+require('dotenv/config');
+const MONGO_DB_URL = process.env.MONGO_DB_URL ? process.env.MONGO_DB_URL : 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 let schema = null;
@@ -12,7 +11,7 @@ async function connection() {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-    .then((conn) => conn.db('model_example'))
+    .then((conn) => conn.db(DB_NAME))
     .then((dbSchema) => {
       schema = dbSchema;
       return schema;
