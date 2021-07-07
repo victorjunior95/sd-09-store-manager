f89acf84d9d508743520e699c85489d55179bb39
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-07-07 19:23:47 -0300

    fix lint

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,11 +1,12 @@
 const createProductsService = require('../services/productsServices');
 
 const OK = 201;
+const INVALID_DATA = 422;
 
 const createProductsController = async (req, res) => {
   const result = await createProductsService(req.body);
 
-  if (result.err) return res.status(422).json(result);
+  if (result.err) return res.status(INVALID_DATA).json(result);
   
   res.status(OK).json(result);
 };
