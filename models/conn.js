a065d3d829ff0991dfbe3908ba8711d590bd770e
Uilton <itoclick@gmail.com>
2021-07-07 22:09:18 -0300

    testes bonus

--- models/conn.js
+++ models/conn.js
@@ -1,12 +1,12 @@
 const { MongoClient } = require('mongodb');
 
 // Para o avaliador funcionar altere a conexão do banco para:
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 const DB_NAME = 'StoreManager';
 
 // A conexão do banco local deverá conter os seguintes parâmetros:
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+//const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
 
 
