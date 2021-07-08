1bbb1875c784804ae450ff9929ad8cb9f958f1c9
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-07-08 17:57:31 -0300

    requisito 1

--- models/connections.js
+++ models/connections.js
@@ -1,12 +1,12 @@
 const mongodb = require('mongodb').MongoClient;
 
 // banco local
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
-const DB_NAME = 'StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// const DB_NAME = 'StoreManager';
 
 // Para o avaliador funcionar
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-// const DB_NAME = 'StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'StoreManager';
 
 module.exports = () =>
   mongodb.connect(MONGO_DB_URL, {
