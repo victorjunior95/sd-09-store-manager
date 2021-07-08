d9e554e164e9c77b7d5f470624bb38a80aae9bb0
brenno-calado <brenno.calado@gmail.com>
2021-07-08 16:52:17 -0300

    remove boom, add connection

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,7 @@
 const rescue = require('express-rescue');
-const boom = require('@hapi/boom');
 const Joi = require('@hapi/joi');
 const services = require('../services/products');
-const { CREATED } = require('./httpCodes');
+const { CREATED, UNPROCESSED } = require('./httpCodes.json');
 const { MIN_STRING, MIN_NUMBER } = require('./magicNumbers.json');
 
 const create = rescue(async (request, response, next) => {
@@ -12,13 +11,16 @@ const create = rescue(async (request, response, next) => {
     quantity: Joi.number().not().empty().min(MIN_NUMBER).required(),
   }).validate(request.body, { convert: false });
 
-  if (error) return response.json(boom.badData(`${error.message}`));
+  if (error) return response.status(UNPROCESSED)
+    .json({ err: { code: 'invalid_data', message: error.message}});
 
   const { name, quantity } = request.body;
 
   const newProduct = await services.create(name, quantity);
+  console.log(newProduct);
 
-  if (newProduct.err) return boom.badData(newProduct.err.message);
+  if (newProduct.err) return response.status(UNPROCESSED)
+    .json({ err: { code: 'invalid_data', message: newProduct.error.message}});
 
   response.status(CREATED).json({
     _id: newProduct._id,
