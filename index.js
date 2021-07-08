eb99e5f4cf739792b8d9fbf242ef045eb111720c
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-08 00:14:12 -0300

    criada rota de post completa, com todas validaçoes necessarias. dando inicio as configuraçoes da GET (products)

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const productsController = require('./controllers/productsController');
+const errorMiddleware = require('./middlewares/error.js');
+const { validationName, validationQuantity } = require('./middlewares/products');
+const app = express();
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.send();
+  response.send('foi');
 });
+
+app.get('/products', productsController.getAll);
+// app.get('/authors/:id', Author.findById);
+app.post('/products',validationName, validationQuantity, productsController.create);
+
+// app.get('/books', Book.getAll);
+// app.get('/books/author/:id', Book.findByAuthorId);
+// app.get('/books/title/:title', Book.findByTitle);
+// app.get('/books/:id', Book.findById);
+// app.post('/books', Book.create);
+
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log('O Pai tá ON lá na 3k!!!'))
--- models/connection.js
+++ models/connection.js
@@ -1,21 +1,22 @@
 const { MongoClient } = require('mongodb');
 
 const OPTIONS = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}
-
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-const DB_NAME = 'StoreManager';
-
-const connection = () => {
-  return db
-    ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-    .then((conn) => {
-      db = conn.db('model_example');
-      return db;
-    })
+  useNewUrlParser:true, 
+  useUnifiedTopology: true
 };
 
+const DB_NAME = 'StoreManager';
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+
+const connection = async () =>
+  MongoClient.connect(MONGO_DB_URL, OPTIONS).then((conn) => conn.db(DB_NAME)
+  ).catch(
+    err => {
+      console.error(err);
+      process.exit(1);
+    }
+  );
+
 module.exports = connection;
+
+//  Uiton Rocha  me ajudou com a conexão
