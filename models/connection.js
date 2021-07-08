92d5fbb3d7fa741a5525c3366c7890b330795a76
brenno-calado <brenno.calado@gmail.com>
2021-07-08 16:52:23 -0300

    remove boom, add connection

---
+++ models/connection.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const MONGO_DB_URL = 'mongodb://localhost:27017';
+
+let db = null;
+
+const connection = () => (
+  db ? Promise.resolve(db)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+      .then((conn) => db = conn.db('StoreManager'))
+);
+
+module.exports = connection;
