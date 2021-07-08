705c5d98bc501db865a21fee67126819206c9ef8
Matheus Martino <matheus.martino97@gmail.com>
2021-07-07 21:05:54 -0300

    Requisito 1.

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,9 +2,9 @@ const productsService = require('../services/productsService');
 
 const add = async (req, res, next) => {
   const { name, quantity } = req.body;
-  const successCode = 201;
+  const success = 201;
   const message = 'Product already exists';
-  const code = 'invalid_data';
+  const errCode = 'invalid_data';
   const errType = 422;
 
   const insertedProduct = await productsService.add(name, quantity);
@@ -12,12 +12,12 @@ const add = async (req, res, next) => {
   if (!insertedProduct) return next({
     err: {
       message,
-      code,
+      code: errCode,
       data: { errType }
     }
   });
 
-  return res.status(successCode).json(insertedProduct);
+  return res.status(success).json(insertedProduct);
 };
 
 module.exports = {
