b3f3572bc214163a1dca974241c218f6dba41864
Lucas Silva <martssilvapro@gmail.com>
2021-07-08 12:33:50 -0300

    corrige erros

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,8 +1,9 @@
+const STATUS_ERR = 422;
 module.exports = (err, _req, res, _next) => {
   // if (err.isJoi) {
   //   return resizeBy.status(422)
   //     .json({ error: { message: err.details[0].message } });
   // }
 
-  res.status(422).json({ err: { code: err.code, message: err.message } });
+  res.status(STATUS_ERR).json({ err: { code: err.code, message: err.message } });
 };
