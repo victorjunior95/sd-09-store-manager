76d8b37a94b1311123eea9b56e68f24e129dbc38
Layo Kaminski <layokaminski@gmail.com>
2021-07-08 13:47:27 -0300

    Ajuste do require do Joi dentro do validProducts

--- middlewares/validProducts.js
+++ middlewares/validProducts.js
@@ -1,11 +1,12 @@
-const Joi = require('joi');
+const Joi = require('@hapi/joi');
 
 const validProducts = (req, _res, next) => {
   const NAME_LENGTH = 5;
+  const MIN_QUANTITY = 1;
   
   const { error } = Joi.object({
     name: Joi.string().min(NAME_LENGTH).not().empty().required(),
-    quantity: Joi.number().not().empty().required()
+    quantity: Joi.number().min(MIN_QUANTITY).not().empty().required()
   }).validate(req.body);
 
   if (error) return next(error);
--- services/ServiceProducts.js
+++ services/ServiceProducts.js
@@ -1,18 +1,6 @@
 const ModelProducts = require('../model/ModelProducts');
 
-const NAME_LENGTH = 5;
-const MIN_QUANTITY = 1;
-
 const create = async ({ name, quantity }) => {
-
-  if (quantity < MIN_QUANTITY) {
-    return {
-      err: {
-        code: 'invalid_data',
-        message: '"quantity" must be larger than or equal to 1',
-      },
-    };
-  }
 
   const findName = await ModelProducts.getByName({ name });
 
