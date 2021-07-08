b978d23d9b81f14bdc32b2a1cd6ba78c715c7baa
victorjunior95 <victorjr.muchacho@gmail.com>
2021-07-08 11:34:07 -0300

    Endpoint for product registration: third correction - challenge 1

--- model/connection.js
+++ model/connection.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 // const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-const DB_NAME = 'StoreManger';
+const DB_NAME = 'StoreManager';
 
 const connection = () => MongoClient
   .connect(MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, })
