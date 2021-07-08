ff777ddf73a31dd86e6b8e15ffbe2b556ce1abc4
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-07-08 11:23:32 -0300

    nodemon

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const app = require('express')();
+const bodyParser = require('body-parser').json();
+
+const PORT = 3000;
+
+app.use(bodyParser);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.post('/products', );
+
+app.listen(PORT, () => console.log(`O pai tá on na porta: ${PORT}`));
