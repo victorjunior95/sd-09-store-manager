8d29466829e5cda2eb0bf41837e1df6a822587af
brenno-calado <brenno.calado@gmail.com>
2021-07-09 18:15:39 -0300

    findById model

--- models/products.js
+++ models/products.js
@@ -1,4 +1,5 @@
 const connection = require('./connection');
+const { ObjectId } = require('mongodb');
 
 const create = async (name, quantity) => (
   await connection().then((db) => db.collection('products').insertOne({ name, quantity }))
@@ -12,4 +13,12 @@ const getAll = async () => (
   await connection().then((db) => db.collection('products').find().toArray())
 );
 
-module.exports = { create, findByName, getAll };
+const findById = async (id) => {
+  if (!ObjectId.isValid(id)) {
+    return { err: { code: 'invalid_data', message: 'Wrong id format'}};
+  }
+
+  return await connection().then((db) => db.collection('products').findOne({ _id: id }));
+};
+
+module.exports = { create, findByName, getAll, findById };
