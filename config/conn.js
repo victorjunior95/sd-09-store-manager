001a1f3ab4489b946e422433a735374900b9ded1
Khyrki <khyrkimitsunobi@gmail.com>
2021-07-08 22:28:07 -0300

    altera utl Mongo

--- config/conn.js
+++ config/conn.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017';
+const MONGO_DB_URL = 'mongodb://mongodb:27017';
 
 const connection = () =>
   MongoClient.connect(MONGO_DB_URL, {
