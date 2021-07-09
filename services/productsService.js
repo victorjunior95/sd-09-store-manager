5f92d0d6af9002f67f1569ff613303fe470bfba4
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-09 11:02:53 -0300

    test: desistindo dos testes unitários

--- services/productsService.js
+++ services/productsService.js
@@ -1,15 +1,37 @@
 const ProductsModel = require('../models/producstModel');
 const httpStatusCode = require('../httpStatusCodes');
 const ApiError = require('../errors/apiError');
+const Joi = require('joi');
 
 
 const create = async ({ name, quantity }) => {
+
+  validateCreatePayload({name, quantity});
 
   validateUniqueProductName(name);
 
   return ProductsModel.create({name, quantity});
 
 };
+
+const validateCreatePayload = (payload) => {
+  const minNameLenght = 5;
+
+  const payloadSchema = Joi.object({
+    name: Joi.string()
+      .min(minNameLenght)
+      .required(),
+    quantity: Joi.number()
+      .integer()
+      .positive()
+      .required()
+  });
+
+  Joi.assert(payload, payloadSchema,
+    {message: '"name" lenght must be at least 5 characters long'});
+};
+
+
 
 const validateUniqueProductName = async (name) => {
   const response = await ProductsModel.findOneByName(name);
