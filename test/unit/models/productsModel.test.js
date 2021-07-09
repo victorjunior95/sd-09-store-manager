adf7b5a7cd3f3cfea2cfdee7e108786f58b86ed5
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-08 22:12:11 -0300

    test: remove name validation test from model role

--- test/unit/models/productsModel.test.js
+++ test/unit/models/productsModel.test.js
@@ -47,16 +47,4 @@ describe('Insert new product into database', () => {
         expect(response).to.deep.include({name: payloadProduct.name, quantity: payloadProduct.quantity})
     });
   });
-
-  describe('When the product name already exists in database', () => {
-    it('Throws an ApiException, with code: "invalid_data", message: Product already existis and statuCode: 422', async () => {
-      try{
-        await ProductsModel.create(payloadProduct);
-        await ProductsModel.create(payloadProduct);
-      } catch(err) {
-        const expectedError = new ApiError('invalid_data', 'Product already existis', httpStatusCode.unprocessableEntity);
-        expect(err).to.include({code: expectedError.code, message: expectedError.message, statusCode: expectedError.statusCode});
-      }
-    })
-  })
 })
