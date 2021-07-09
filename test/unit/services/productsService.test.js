d0523ec410bc5aa76c2098a04223459ff5a1dfe8
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-08 22:12:54 -0300

    test: fix model mocks

--- test/unit/services/productsService.test.js
+++ test/unit/services/productsService.test.js
@@ -1,22 +1,36 @@
 const sinon = require('sinon');
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
+const { expect } = require('chai');
 
 const httpStatusCode = require('../../../httpStatusCodes');
 const ApiError = require('../../../errors/apiError');
-const ProductsService = require('../../../services/producstServices');
-chai.use(chaiAsPromised);
+const ProductsService = require('../../../services/productsService');
+const ProductsModel = require('../../../models/producstModel');
 
-const expect = chai.expect;
+const payloadProduct = {
+  name: "led rgb",
+  quantity: 10,
+}
 
 describe('Insert new product into database', () => {
 
   describe('When it is successfully inserted', () => {
 
-    const payloadProduct = {
-      name: "led rgb",
-      quantity: 10,
-    }
+    beforeEach(() => {
+      const mockResponse = {
+        _id: "604cb554311d68f491ba5781",
+        name: "led rgb",
+        quantity: 10,
+      }
+
+      sinon.stub(ProductsModel, 'create').resolves(mockResponse);
+      sinon.stub(ProductsModel, 'findOneByName').resolves(null);
+
+    });
+
+    afterEach(() => {
+      ProductsModel.create.restore();
+      ProductsModel.findOneByName.restore();
+    });
 
     it('Returns an object', async () => {
         const response = await ProductsService.create(payloadProduct);
@@ -31,12 +45,29 @@ describe('Insert new product into database', () => {
   });
 
   describe('When the product name already exists in database', () => {
-    it('Throws an ApiException, with code: "invalid_data", message: Product already existis and statuCode: 422', async () => {
+
+    beforeEach(() => {
+      const mockResponse = {
+        _id: "604cb554311d68f491ba5781",
+        name: "led rgb",
+        quantity: 10,
+      }
+
+      sinon.stub(ProductsModel, 'create').resolves(mockResponse);
+      sinon.stub(ProductsModel, 'findOneByName').resolves(mockResponse);
+
+    });
+
+    afterEach(() => {
+      ProductsModel.create.restore();
+      ProductsModel.findOneByName.restore();
+    });
+
+    it('Throws an ApiException, with code: "invalid_data", message: "Product already exists" and statuCode: "422"', async () => {
       try{
         await ProductsService.create(payloadProduct);
-        await ProductsService.create(payloadProduct);
       } catch(err) {
-          const expectedError = new ApiError('invalid_data', 'Product already existis', httpStatusCode.unprocessableEntity);
+          const expectedError = new ApiError('invalid_data', 'Product already exists', httpStatusCode.unprocessableEntity);
           expect(err).to.include({code: expectedError.code, message: expectedError.message, statusCode: expectedError.statusCode});
       }
     })
