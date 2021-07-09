fcca6c1492e3fb491fc3ff26c851ccbd7b887690
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-09 03:47:53 -0300

    formataçao

--- middlewares/products.js
+++ middlewares/products.js
@@ -1,39 +1,43 @@
+const minimalQuantity = 0,
+  minimalChars = 5,
+  STATUS_422 = 422;
+
 const validationName = (req, res, next) => {
-    const name = req.body.name;
-    if (name.length < 5) {
-        res.status(422).json({
-            err: {
-                code: "invalid_data",
-                message: '"name" length must be at least 5 characters long',
-            }
-        });
-    }
-    return next();
+  const name = req.body.name;
+  if (name.length < minimalChars) {
+    res.status(STATUS_422).json({
+      err: {
+        code: 'invalid_data',
+        message: '"name" length must be at least 5 characters long',
+      }
+    });
+  }
+  return next();
 };
 
 const validationQuantity = (req, res, next) => {
-    const quantity = req.body.quantity;
-   if (typeof(quantity) !== "number"){
-    res.status(422).json({
-        err: {
-            code: 'invalid_data',
-            message: '"quantity" must be a number',
-          },
+  const quantity = req.body.quantity;
+  if (typeof (quantity) !== 'number') {
+    res.status(STATUS_422).json({
+      err: {
+        code: 'invalid_data',
+        message: '"quantity" must be a number',
+      },
     });
-   }
-    if (quantity <= 0 || quantity % 1 !== 0) {
-        res.status(422).json({
-            err: {
-                code: 'invalid_data',
-                message: '"quantity" must be larger than or equal to 1',
-              },
-        });
-    }
-    return next();
+  }
+  if (quantity <= minimalQuantity|| quantity % 1 !== minimalQuantity) {
+    res.status(STATUS_422).json({
+      err: {
+        code: 'invalid_data',
+        message: '"quantity" must be larger than or equal to 1',
+      },
+    });
+  }
+  return next();
 };
 
 
 module.exports = {
-    validationName,
-    validationQuantity
+  validationName,
+  validationQuantity
 };
--- services/products.js
+++ services/products.js
@@ -24,26 +24,7 @@ const change = async (id, name, quantity) => {
   return product;
 };
 
-  // const create = async (firstName, middleName, lastName) => {
-  //   // Buscamos um autor com o mesmo nome completo que desejamos criar
-  //   const existingAuthor = await Author.findByName(firstName, middleName, lastName);
-  
-  //   // Caso esse autor já exista, retornamos um objeto de erro informando
-  //   // que não é possível criar o autor pois ele já existe
-  //   if (existingAuthor) {
-  //     return {
-  //       error: {
-  //         code: 'alreadyExists',
-  //         message: 'Um autor já existe com esse nome completo',
-  //       },
-  //     };
-  //   }'
-  
-  //   // Caso o autor não exista e, portanto, possa ser criado
-  //   // chamamos o model e retornamos o resultado
-  //   return Author.create(firstName, middleName, lastName);
-  // };
-  
+
 module.exports = {
   getAll,
   del,
