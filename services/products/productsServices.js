1156c7e0ce226753f7fda9d1185ff057678f1ce3
Enio Nicael <enionicael@gmail.com>
2021-07-09 15:31:41 -0300

    Requisito 1, implementacao das validacoes.

--- services/products/productsServices.js
+++ services/products/productsServices.js
@@ -1,14 +1,18 @@
 const productsModel = require('../../models/products/productsModel');
 
-const validate = (name, quantity) => {
+const validate = async (name, quantity) => {
   if (name.length < 5) return { status: 422, code: 'invalid_data', message: '"name" length must be at least 5 characters long' };
-  if (quantity < 0) return { status: 422, code: 'invalid_data', message: '"name" length must be at least 5 characters long' };
+  if (quantity < 1) return { status: 422, code: 'invalid_data', message: '"quantity" must be larger than or equal to 1' };
+  if (typeof quantity === 'string') return { status: 422, code: 'invalid_data', message: '"quantity" must be a number' };
+
+  const produto = await productsModel.searchProductName(name);
+  if (produto) return { status: 422, code: 'invalid_data', message: 'Product already exists' };
 
   return {};
 };
 
 const create = async (name, quantity) => {
-  const validations = validate(name, quantity);
+  const validations = await validate(name, quantity);
   if (validations.message) return validations;
 
   // name deve ser uma string com mais de 5 caracteres
@@ -19,8 +23,8 @@ const create = async (name, quantity) => {
 
   //
 
-  const produto = await productsModel.searchProductName(name);
-  if (produto) return { status: 422, code: 'invalid_data', message: 'Product already exists' };
+  /* const produto = await productsModel.searchProductName(name);
+  if (produto) return { status: 422, code: 'invalid_data', message: 'Product already exists' }; */
 
   const newProduct = await productsModel.createProducts(name, quantity);
   // console.log(newProduct);
