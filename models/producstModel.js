a0f7230cd342fd00a9a00da321e751d130700c96
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-08 22:11:15 -0300

    fix: remove name validation from model role

--- models/producstModel.js
+++ models/producstModel.js
@@ -1,28 +1,27 @@
-const httpStatusCode = require('../httpStatusCodes');
 const connection = require('./connection');
-const ApiError = require('../errors/apiError');
 
 const create = async ({ name, quantity }) => {
   const productsCollection = await connection()
     .then((db) => db.collection('products'));
-
-  await validateUniqueProductName(name, productsCollection);
 
   const response = await productsCollection.insertOne({ name, quantity });
   const inserted = response.ops[0];
   return inserted;
 };
 
-const validateUniqueProductName = async (name, productsCollection) => {
+const findOneByName = async (name) => {
+  const productsCollection = await connection()
+    .then((db) => db.collection('products'));
+
   const response = await productsCollection.findOne({name});
 
-  if (!response) return;
+  if (response) return response.ops[0];
 
-  throw new ApiError('invalid_data', 'Product already existis',
-    httpStatusCode.unprocessableEntity);
+  return response;
 };
 
 
 module.exports = {
   create,
+  findOneByName
 };
