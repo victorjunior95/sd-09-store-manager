a261ccc593cfad6b23ef743c7ec49da4c86bce79
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-09 17:59:49 -0300

    feat: validade requisition payload

--- services/productsService.js
+++ services/productsService.js
@@ -1,23 +1,38 @@
 const ProductsModel = require('../models/producstModel');
+const Joi = require('@hapi/joi');
 const httpStatusCode = require('../httpStatusCodes');
 const ApiError = require('../errors/apiError');
 
-
 const create = async ({ name, quantity }) => {
+  validateCreatePayload({ name, quantity });
 
-  validateUniqueProductName(name);
+  await validateUniqueProductName(name);
 
-  return ProductsModel.create({name, quantity});
-
+  return ProductsModel.create({ name, quantity });
 };
 
 const validateUniqueProductName = async (name) => {
   const response = await ProductsModel.findOneByName(name);
 
-  if (!response) return;
+  if (response)
+    throw new ApiError(
+      'invalid_data',
+      'Product already exists',
+      httpStatusCode.unprocessableEntity,
+    );
+};
 
-  throw new ApiError('invalid_data', 'Product already exists',
-    httpStatusCode.unprocessableEntity);
+const validateCreatePayload = ({ name, quantity }) => {
+  const minNameLength = 5;
+  const payloadSchema = Joi.object({
+    name: Joi.string().min(minNameLength).required(),
+    quantity: Joi.number().integer().positive().required(),
+  });
+
+  const { error } = payloadSchema.validate({ name, quantity });
+
+  if (error)
+    throw new ApiError('invalid_data', error.message, httpStatusCode.unprocessableEntity);
 };
 
 module.exports = {
