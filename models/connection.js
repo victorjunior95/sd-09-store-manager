82081de66df97650bb8924444431c4d3d584265d
rodolfo_oliveira <rod.vgo@gmail.com>
2021-07-10 08:11:43 -0300

    req 1 fix

--- models/connection.js
+++ models/connection.js
@@ -5,8 +5,8 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
 let db = null;
 
 const DB_NAME = 'StoreManager';
