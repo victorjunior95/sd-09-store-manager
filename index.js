d0579cf8b462c6652be5c625da7a7481cc1e38bf
Nathália <nathaliaveneziano@icloud.com>
2021-07-10 16:57:20 -0300

    Iniciando projeto

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ models/Products.js
@@ -0,0 +1,4 @@
+/*
+products
+{ "_id": ObjectId("5f43cbf4c45ff5104986e81d"), "name": "Produto Silva", "quantity": 10 }
+*/
