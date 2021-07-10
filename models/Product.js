d9d36aba3cb2d56052705de8ea182f58259550d1
Raquel Picanço <picanco.raquel@gmail.com>
2021-07-10 17:49:59 -0400

    create products update (model) + tests

--- models/Product.js
+++ models/Product.js
@@ -27,6 +27,23 @@ class Product {
       .then((collection) => collection.find({ _id: ObjectId(id) }).toArray())
       .then((result) => result.length ? result[0] : []);
   }
+
+  async update(payload) {
+    const { id } = payload;
+    if (!ObjectId.isValid(id)) return null;
+
+    delete payload.id;
+
+    return connection()
+      .then((db) => db.collection(this.collection))
+      .then(
+        (collection) => collection.findOneAndUpdate(
+          { _id: ObjectId(id) },
+          { $set: payload },
+          { returnOriginal: false }
+        )
+      ).then((result) => !result.value ? {} : result.value );
+  }
 }
 
 module.exports = Product;
