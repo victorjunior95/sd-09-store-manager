d1ac6a78b1e23efcbc405f2d40dc45ae76bdd46a
genadireuben <genadireuben91@gmail.com>
2021-07-11 20:46:26 -0300

    implementa endpoints de sales

--- routes/router.js
+++ routes/router.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const ProductRouter = require('../controllers/ProductController');
 
+const SalesRouter = require('../controllers/SalesController');
+
 const router = express.Router();
 
 router.use('/products', ProductRouter);
+
+router.use('/sales', SalesRouter);
 
 module.exports = router;
