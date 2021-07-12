7744123968efd537844fe527fcea2631c916b246
Pablo Fernandes <pabloprates@gmail.com>
2021-07-12 08:42:22 -0300

    Changing the connection to run at evaluator server

--- models/connection.js
+++ models/connection.js
@@ -5,14 +5,16 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'StoreManager';
 let db = null;
 
 const connection = () => {
   return db
     ? Promise.resolve(db)
-    : MongoClient.connect(process.env.DB_URL, OPTIONS)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
       .then((conn) => {
-        db = conn.db(process.env.DB_NAME);
+        db = conn.db(DB_NAME);
         return db;
       });
 };
