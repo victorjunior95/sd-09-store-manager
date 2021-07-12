15adf7df0b682318989ec93c0c8e3f43c48ef3c2
Renato Feitosa <pythaqua@gmail.com>
2021-07-11 23:03:14 -0300

    chore: Set initial server

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const DEFAULT_PORT = 3000;
+const PORT = process.env.PORT || DEFAULT_PORT;
+
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`Connected at port ${PORT}`));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
