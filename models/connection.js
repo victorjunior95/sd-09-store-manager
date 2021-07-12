f8383c1f1c9e6a97cda4086f50af876b0a8386fd
edison-ps <edison@osite.com.br>
2021-07-12 12:05:02 -0300

    Requisito 8 V1.01

--- models/connection.js
+++ models/connection.js
@@ -1,10 +1,10 @@
 const { MongoClient } = require('mongodb');
 
 // Teste local
-// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
 // Teste avaliador
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 const DB_NAME = 'StoreManager';
 
