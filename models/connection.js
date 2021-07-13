006b07ab1a0bab865654b4990909c2c0230a686c
brenno-calado <brenno.calado@gmail.com>
2021-07-12 21:53:20 -0300

    change mongodb URL

--- models/connection.js
+++ models/connection.js
@@ -5,14 +5,16 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://localhost:27017';
+// const LOCAL_MONGO_DB_URL = 'mongodb://localhost:27017';
+const MONGO_DB_URL = 'mongodb://mongodb:27017';
+const DB_NAME = 'StoreManager';
 
 let db = null;
 
 const connection = () => (
   db ? Promise.resolve(db)
     : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-      .then((conn) => db = conn.db('StoreManager'))
+      .then((conn) => db = conn.db(DB_NAME))
 );
 
 module.exports = connection;
