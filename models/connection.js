4b4f97124cfdb36366f9b511189f84d4bd5e1434
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-07-14 19:10:11 -0300

    Requisito 01

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,8 @@
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
-let db = null;
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// let db = null;
 
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-// let db = 'StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+let db = 'StoreManager';
 
 const { MongoClient } = require('mongodb');
 
