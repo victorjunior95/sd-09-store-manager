ef9ff646ee2282d339a66cce673b69f6dcfe181c
brenno-calado <brenno.calado@gmail.com>
2021-07-13 22:37:38 -0300

    evaluator URL

--- models/connection.js
+++ models/connection.js
@@ -5,15 +5,15 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const LOCAL_MONGO_DB_URL = 'mongodb://localhost:27017';
-// const MONGO_DB_URL = 'mongodb://mongodb:27017';
+// const LOCAL_MONGO_DB_URL = 'mongodb://localhost:27017';
+const MONGO_DB_URL = 'mongodb://mongodb:27017';
 const DB_NAME = 'StoreManager';
 
 let db = null;
 
 const connection = () => (
   db ? Promise.resolve(db)
-    : MongoClient.connect(LOCAL_MONGO_DB_URL, OPTIONS)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
       .then((conn) => db = conn.db(DB_NAME))
 );
 
