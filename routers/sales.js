8f20732b601ea9e2758b9e42271a97e3a79b1745
brenno-calado <brenno.calado@gmail.com>
2021-07-13 22:39:54 -0300

    delete sale route

--- routers/sales.js
+++ routers/sales.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { create, getAll, findById, update } = require('../controllers/sales');
+const { create, getAll, findById, update, del } = require('../controllers/sales');
 const sales = express.Router();
 
 sales.post('/', create);
@@ -9,5 +9,7 @@ sales.get('/', getAll);
 sales.get('/:id', findById);
 
 sales.put('/:id', update);
+
+sales.delete('/:id', del);
 
 module.exports = sales;
