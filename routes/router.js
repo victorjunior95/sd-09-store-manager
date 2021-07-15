2f5b5d08c9963b446cb05862ea42963842000f31
Pedro <ped.alm2709@gmail.com>
2021-07-15 20:25:24 -0300

    router

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
-const ProductController = require('../controllers/ProductController');
-const SaleController = require('../controllers/SaleController');
+const ProductController = require('../controllers/productsController');
+const SaleController = require('../controllers/salesController');
 
 const router = Router();
 
