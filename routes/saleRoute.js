be6393a8e152cc540bf5ea14fef413ea99612ce8
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-07-16 07:21:53 -0300

    req 8

--- routes/saleRoute.js
+++ routes/saleRoute.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const saleController = require('../controllers/saleController');
-const productRoute = require('./productRoute');
 const saleRoute = express.Router();
 
 // cria nova vendas
