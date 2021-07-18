0e30ee30259192c3069f3f06b591949f415352d5
Isabella Paz <bellapaz.iorp@gmail.com>
2021-07-18 20:45:27 -0300

    controllers dos produtos

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-const productsControllers = require('./productsControllers');
-const salesControllers = require('./salesControllers');
-
-module.exports = {
-  productsControllers,
-  salesControllers,
-};
