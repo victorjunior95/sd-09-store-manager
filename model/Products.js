e50d5c40c27ed478b7281de22014b5fffae5814d
ViniciusChagas <viniciushrcs@gmail.com>
2021-07-19 21:07:11 -0300

    Validações para a criação de um novo produto

--- model/Products.js
+++ model/Products.js
@@ -5,6 +5,14 @@ const createNewProduct = async (name, quantity) =>
     .then((db) => db.collection('products').insertOne({ name, quantity }))
     .then((result) => (result.ops[0])); //ou { _id: result.insertedId, name, quantity }
 
+const findByName = async (name) => {
+  const result = await connection()
+    .then((db) => db.collection('products').findOne({ name }));
+  if (!result) return null;
+  return result;
+};
+
 module.exports = {
   createNewProduct,
+  findByName,
 };
--- service/Products.js
+++ service/Products.js
@@ -1,7 +1,67 @@
 const Products = require('../model/Products');
 
+const checkNameLength = (name) => {
+  const minNameLength = 5;
+  if (name.length < minNameLength) {
+    throw { 
+      err: {
+        code: 'invalid_data',
+        message: '"name" length must be at least 5 characters long'
+      },
+      status: 422,
+    };
+  }
+};
+
+const checkQuantity = (quantity) => {
+  if (quantity < 1) {
+    throw { 
+      err: {
+        code: 'invalid_data',
+        message: '"quantity" must be larger than or equal to 1'
+      },
+      status: 422,
+    };
+  }
+};
+
+const checkQuantityType = (quantity) => {
+  if (typeof quantity !== 'number') {
+    throw { 
+      err: {
+        code: 'invalid_data',
+        message: '"quantity" must be a number'
+      },
+      status: 422,
+    };
+  }
+};
+
+const checkProductIsValid = (name, quantity) => {
+  checkNameLength(name);
+  checkQuantity(quantity);
+  checkQuantityType(quantity);
+};
+
+const checkIfNameExists = async (name) => {
+  const isProduct = await Products.findByName(name);
+
+  if (isProduct) {
+    throw { 
+      err: {
+        code: 'invalid_data',
+        message: 'Product already exists'
+      },
+      status: 422,
+    };
+  }
+};
+
+
 const createNewProduct = async (name, quantity) => {
-	 const newProduct = await Products.createNewProduct(name, quantity);
+	 await checkIfNameExists(name);
+	 checkProductIsValid(name, quantity); 
+ 	const newProduct = await Products.createNewProduct(name, quantity);
   return {
     status: 201,
     newProduct
