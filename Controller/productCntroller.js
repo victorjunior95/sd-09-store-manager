1ca0028a707d020e94b41a57440d2705a5f84b67
Hudson Carlos <hc88806@gmail.com>
2021-07-21 10:35:36 -0400

    endpoint registration product done

--- Controller/productCntroller.js
+++ Controller/productCntroller.js
@@ -1,13 +1,13 @@
 const productServeci = require('../Service/productService');
 const rescue = require('express-rescue');
-const status = 201;
+const stt = 201;
 
 const create = rescue(
   async (req, res) => {
     const { name, quantity } = req.body;
     const retorneCreate = await productServeci.create(name, quantity);
 
-    return res.status(status).json(retorneCreate);
+    return res.status(stt).json(retorneCreate);
 
   }
 );
