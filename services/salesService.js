942cee5491e43fd60f8724e58d381d91d31b6a93
Alana Monteiro <lanaah.15@gmail.com>
2021-07-21 10:56:59 -0300

    ajuste url connection

--- services/salesService.js
+++ services/salesService.js
@@ -1,7 +1,8 @@
 const { createSalesModel } = require('../models/sales');
 const joi = require('joi');
 
-//Criar as verificações para o requisito 5.
+// Criar as verificações para o requisito 5.
+// Verificar push no github
 
 const createSales = async (salesData) => {
   const create = await createSalesModel(salesData);
