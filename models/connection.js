c50917187b65bb876536bc1448f1e9101b807c8e
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-07-21 11:29:46 -0300

    requisito 13 feito

--- models/connection.js
+++ models/connection.js
@@ -4,18 +4,7 @@ const OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
 const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
-let db = null;
-
-const connection = () => db
-  ? Promise.resolve(db)
-  : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-    .then((conn) => {
-      db = conn.db(DB_NAME);
-      return db;
-    })
-    .catch((err) => {
-      console.error(err);
-      process.exit();
-    });
+const connection = () => MongoClient.connect(MONGO_DB_URL, OPTIONS)
+  .then((conn) => conn.db(DB_NAME));
 
 module.exports = connection;
--- utils/validators.js
+++ utils/validators.js
@@ -1,47 +1,39 @@
 const products = require('../models/products');
 const { ObjectID } = require('mongodb');
 
+const err = (code, message) => ({ code, message });
+
 const product = async ({ name, quantity }) => {
   const min = 5;
   let message;
   if (name.length < min) message = '"name" length must be at least 5 characters long';
   if (typeof(quantity) !== 'number') message = '"quantity" must be a number';
   if (quantity < 1) message = '"quantity" must be larger than or equal to 1';
-  if (message) throw { code: 'invalid_data', message };
+  if (message) throw err('invalid_data', message);
 };
 
 const productExists = async ({ name }) => {
   const exists = await products.getByName(name);
-  if (exists) throw {
-    code: 'invalid_data', message: 'Product already exists'
-  };
+  if (exists) throw err('invalid_data', 'Product already exists');
 };
 
 const productId = async (id) => {
-  if (!ObjectID.isValid(id)) throw {
-    code: 'invalid_data', message: 'Wrong id format'
-  };
+  if (!ObjectID.isValid(id)) throw err('invalid_data', 'Wrong id format');
 };
 
 const sale = async (itensSold) => {
   const minLength = 0;
   const isValid = itensSold.every(({ quantity }) =>
     (typeof(quantity) === 'number' && quantity > minLength));
-  if (!isValid) throw {
-    code: 'invalid_data', message: 'Wrong product ID or invalid quantity'
-  };
+  if (!isValid) throw err('invalid_data', 'Wrong product ID or invalid quantity');
 };
 
 const saleExists = async (id) => {
-  if (!ObjectID.isValid(id)) throw {
-    code: 'not_found', message: 'Sale not found'
-  };
+  if (!ObjectID.isValid(id)) throw err('not_found', 'Sale not found');
 };
 
 const saleId = async (id) => {
-  if (!ObjectID.isValid(id)) throw {
-    code: 'invalid_data', message: 'Wrong sale ID format'
-  };
+  if (!ObjectID.isValid(id)) throw err('invalid_data', 'Wrong sale ID format');
 };
 
 const stock = async (itensSold) => {
@@ -50,9 +42,7 @@ const stock = async (itensSold) => {
     const stock = arr.find((e) => e._id.toString() === productId);
     return stock.quantity >= quantity;
   });
-  if (!available) throw {
-    code: 'stock_problem', message: 'Such amount is not permitted to sell'
-  };
+  if (!available) throw err('stock_problem', 'Such amount is not permitted to sell');
 };
 
 module.exports = { product, productExists, productId, sale, saleExists, saleId, stock };
