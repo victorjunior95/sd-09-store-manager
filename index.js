9ccfb558d2c078c9a0f09662d7a7cabb72c86c19
Kevin Fraga <kevinciolac@gmail.com>
2021-07-31 18:26:23 -0300

    structure done

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const HTTP_OK_STATUS = 200;
+
+app.use(bodyParser.json());
+
+app.get('/teste', (_req, res) => {
+  res.status(HTTP_OK_STATUS).json({ message: 'Tested' });
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+const localhost = 3000;
+
+const PORT = process.env.PORT || localhost;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
 });
---
+++ test/unit/connectionMock.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const DB_NAME = 'StoreManager';
+
+const getConnection = async () => {
+  const DBServer = await MongoMemoryServer.create();
+  const URLMock = DBServer.getUri();
+  const OPTIONS = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  return MongoClient.connect(URLMock, OPTIONS)
+    .then((conn) => conn.db(DB_NAME));
+};
+
+module.exports = { getConnection };
