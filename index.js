d9e415faae342c64d4ef71e5c4f4dc5636d54f89
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-08-03 15:36:48 -0300

    Estrutura inicial: pastas controller, models e services. Conexao com o bd com o models/connections

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+const LOCALHOST = 3000;
+
+const port = process.env.PORT || LOCALHOST;
+
+app.listen(port, () => { console.log(`Ouvindo na porta ${port}`);});
