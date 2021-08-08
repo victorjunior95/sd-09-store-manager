3ae9310592d10b14a04934dd03c07a83a64c1c17
Victor <moraesvictorgk@gmail.com>
2021-08-08 02:42:50 -0300

    feat(sales): update /create function/ at services

--- services/Sales.js
+++ services/Sales.js
@@ -1,4 +1,6 @@
 const Joi = require('joi');
+const stock = require('../models/Products');
+const model = require('../models/Sales');
 
 const UNAVAIALBLE_REQUISITION = 422;
 
@@ -9,7 +11,7 @@ const schema = Joi.array().items({
 
 });
 
-const create = (item) => {
+const create = async (item) => {
   const { error } = schema.validate(item);
 
   if(error) {
@@ -18,10 +20,28 @@ const create = (item) => {
         code: 'invalid_data',
         error: {message: 'Wrong product ID or invalid quantity'}
       },
-      status: UNAVAIALBLE_REQUISITION,
+      status: 422
       
     };   
   }
+  const { productId } = item[0];
+  const { name, quantity } = await stock.findById(productId);
+  const itemQuantity = quantity - item[0].quantity;
+  const NO_STOCK = 0;
+
+  if (itemQuantity < NO_STOCK) {
+    return {
+      status: NOT_FOUND,
+      code: 'stock_problem',
+      error: { message: 'Such amount is not permitted to sell' }
+    };
+  }
+
+  await stock.updateProduct(productId, name, itemQuantity);
+  const newSale = await model.create(item);
+
+
+  return newSale;
 };
 
 module.exports = {
