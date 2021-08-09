7ed350295636843dd0c82bdb932293f12b0efc9d
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-08-09 03:23:31 -0300

    adiciona error middleware

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const Products = require('./controllers/Products');
+const errorMiddleware = require('./middlewares/error');
+
 app.use(express.json());
+app.use(errorMiddleware);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.post('/products', (req, res) => {
-  const {name, quantity} = req.body;
-  res.status(200).send({name, quantity});
-});
+app.post('/products', Products.create);
 
 
 app.listen(PORT, () => console.log(`Ouvindo a porta ${PORT}`));
