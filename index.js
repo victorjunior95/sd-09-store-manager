977e1e711cfc42ef835d9635458bfcf29ade7a75
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-08-09 02:44:13 -0300

    Adiciona o básico do index

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Ouvindo a porta ${PORT}`));
