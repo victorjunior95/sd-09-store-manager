5e3db981b2b2f0561fc505e516aadc140baefaf1
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-08-09 02:48:50 -0300

    Adiciona URL para uso do avaliador

--- models/connection.js
+++ models/connection.js
@@ -7,7 +7,7 @@ const OPTIONS = {
 
 let db = null;
 
-const MONGO_DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = process.env.DB_URL || 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 const connection = () => {
