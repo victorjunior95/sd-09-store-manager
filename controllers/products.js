396d1e51e643b0439dc455a97cffa25be4d4f6ee
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-08-09 11:50:22 -0300

    adding products controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,1 +1,29 @@
+const products = require('../services/products');
 
+const postProduct = (request, response) => products
+  .postProduct(request.body)
+  .then(({status, data}) => response.status(status).json(data));
+
+const getAllProducts = (request, response) => products
+  .getAllProducts(request.body)
+  .then(({status, data}) => response.status(status).json(data));
+
+const getProductById = (request, response) => products
+  .getProductById(request.params.id, request.body)
+  .then(({status, data}) => response.status(status).json(data));
+
+const putProduct = (request, response) => products
+  .putProduct(request.body)
+  .then(({status, data}) => response.status(status).json(data));
+
+const deleteProduct = (request, response) => products
+  .deleteProduct(request.body)
+  .then(({status, data}) => response.status(status).json(data));
+
+module.exports = {
+  postProduct,
+  getAllProducts,
+  getProductById,
+  putProduct,
+  deleteProduct,
+};
